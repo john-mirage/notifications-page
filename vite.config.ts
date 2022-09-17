0123e824d3bcaf949a2bbd4e9d494bf33aa90792
john-mirage <chaosdynamix@posteo.net>
2022-09-17 09:10:16 +0200

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/notifications-page/",
+  resolve: {
+    alias: {
+      "@components": resolve(__dirname, "src/components"),
+      "@fonts": resolve(__dirname, "src/fonts"),
+      "@images": resolve(__dirname, "src/images"),
+      "@styles": resolve(__dirname, "src/styles"),
+    },
+  },
+});
